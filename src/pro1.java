820c01c8e95e5a853620c425de2318740d8783b3
sbshivam04 <sbshivam04@gmail.com>
2019-01-10 19:53:16 -0800

    added

---
+++ src/pro1.java
@@ -0,0 +1,13 @@
+class pro1
+{
+  public static void main(String args[]) 
+  {
+    int a,b,c; // this declares a variable called num
+    a = 100;
+    b=50; // this assigns num the value 100
+    System.out.print("This is a :" +a);
+    System.out.print("This is b :" +b);
+    c=a+b;
+    System.out.print("This is sum:" +c);
+  }
+}
